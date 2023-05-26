b43fc52cb9291b25073d2440d953928a91916979
Arjun singh <79888586+Arjunsingh606@users.noreply.github.com>
2023-05-26 11:59:50 +0530

    3rd commit

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './cart.css'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { remove } from '../store/cartSlice';
 
 const Cart = () => {
+ const dispatch = useDispatch();
  const products = useSelector(state => state.cart)
 
-
+ const handleRemove = (productId)=>{
+     dispatch(remove(productId))
+}
   return (
     <>
        <h3>Cart Items</h3>
@@ -17,7 +21,7 @@ const Cart = () => {
                <img src={product.image} alt="load ho rahi hai" />
                <h5>{product.title}</h5>
                <h5>{product.price}</h5>
-               <button className="cart-btn">Remove</button>
+               <button onClick={handleRemove(product.id)} className="cart-btn">Remove</button>
               </div>
              ))
            }
